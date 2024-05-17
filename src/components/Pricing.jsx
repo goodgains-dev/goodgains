1bc09e0f8582982ee20b46ee49bcd46259718217
goodgains-dev <159225098+goodgains-dev@users.noreply.github.com>
2024-05-16 20:06:31 -0400

    Update Pricing.jsx
    1

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -28,7 +28,7 @@ const Pricing = () => {
         </div>
 
         <Heading
-          tag="Get started with Brainwave"
+          tag="Get started with GoodGains"
           title="Pay once, use forever"
         />
 
