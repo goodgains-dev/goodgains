94f898ebd551ac27fd76c1a7042ffd582c5fe7eb
goodgains-dev <159225098+goodgains-dev@users.noreply.github.com>
2024-05-16 22:59:31 -0400

    Update Hero.jsx
    1
    
    Signed-off-by: goodgains-dev <159225098+goodgains-dev@users.noreply.github.com>

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -48,7 +48,7 @@ const Hero = () => {
             <div className="relative bg-n-8 rounded-[1rem]">
               <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
 
-              <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
+              <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[688/490]">
                 <img
                   src={robot}
                   className="w-full scale-[1.0] translate-y-[10%] md:scale-[1.0] md:-translate-y-[10%] lg:-translate-y-[23%]"
