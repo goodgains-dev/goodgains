a48175323bf85ccf2ba358a40ba3fdd78316ed22
goodgains-dev <159225098+goodgains-dev@users.noreply.github.com>
2024-05-18 13:42:03 -0400

    Update Benefits.jsx
    1
    
    Signed-off-by: goodgains-dev <159225098+goodgains-dev@users.noreply.github.com>

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -33,6 +33,10 @@ const Benefits = () => {
                     height={48}
                     alt={item.title}
                   />
+                  <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
+                  </p>
+                  <Arrow />
+                </div>
                 </div>
               </div>
 
