dc2f34610292b9e7dc65b852961726375d17b6bd
GoatsOnTheMoon <121653725+GoatsOnTheMoon@users.noreply.github.com>
2024-07-08 12:45:47 -0400

    Update Hero.jsx
    Signed-off-by: GoatsOnTheMoon <121653725+GoatsOnTheMoon@users.noreply.github.com>

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
       <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
-            Explore the Possibilities of blockchain by Chatting with {` `}
+            Where Communication Meets Innovation: {` `}
             <span className="inline-block relative">
               GoodGains{" "}
               <img
@@ -36,8 +36,7 @@ const Hero = () => {
             </span>
           </h1>
           <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
-            Utilize blockchain technology like never before. Upgrade your productivity
-            with GoodGains, your digital third place.
+            Join us in creating your digital third place.
           </p>
           <Button href="https://goodgainsgram.vercel.app/" white>
             Get started
